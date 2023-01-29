c0772814fc3ea52b72fac7c94c715caf8a869cb4
Hanbyoul <dytjqdla120@naver.com>
2023-01-29 23:04:28 +0900

    react hooks practice

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
 import React, { useEffect, useRef, useState } from "react";
-
-const useNetwork = (onChange) => {
-  const [status, setStatus] = useState(navigator.onLine); //true & false
-  const handleChange = () => {
-    if (typeof onChange === "function") {
-      onChange(navigator.onLine);
-    }
-    setStatus(navigator.onLine);
-  };
-
-  useEffect(() => {
-    window.addEventListener("online", handleChange);
-    window.addEventListener("offline", handleChange);
-
-    return () => {
-      window.removeEventListener("online", handleChange);
-      window.removeEventListener("offline", handleChange);
-    };
-  }, []);
-
-  return status;
-};
+import useAxios from "./useAxios";
 
 function App() {
-  const handleNetworkChange = (onlinehehe) => {
-    console.log(onlinehehe ? "we just went online" : "we are offline");
-  };
-
-  const onLine = useNetwork(handleNetworkChange);
-
+  const { loading, data, error, refetch } = useAxios({
+    url: "https://yts.mx/api/v2/list_movies.json",
+  });
+  console.log(
+    `Loading:${loading} \nError:${error}\nData:${JSON.stringify(data)}`
+  );
   return (
     <div>
-      <h1>{onLine ? "onLine" : "OffLine"}</h1>
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
     </div>
   );
 }
